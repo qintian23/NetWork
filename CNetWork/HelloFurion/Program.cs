bc012087affeccafc71122f456f663f6c941f3a1
乐 简 <2576950110@qq.com>
2022-07-28 11:17:13 +0800

    Furion好例子

--- CNetWork/HelloFurion/Program.cs
+++ CNetWork/HelloFurion/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Serve.Run("http://localhost:25161");
+
+Serve.Run(RunOptions.Default.ConfigureBuilder(builder =>
+    {
+        builder.WebHost.UseSetting("UseUrls","http://localhost:25161"); // 不行啊
+    }
+));
+
+[DynamicApiController]
+public class HelloService
+{
+    public string Say()
+    {
+        return "Hello, Furion";
+    }
+}
+
+// 同时支持dotnet run 和 dotnet watch run
