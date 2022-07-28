1c21768d6c5cbc7880b8869d71af407966315f9e
乐 简 <2576950110@qq.com>
2022-07-28 14:45:55 +0800

    小修改

--- FurionSqlSugar/FurionSqlSugar.Web.Entry/Program.cs
+++ FurionSqlSugar/FurionSqlSugar.Web.Entry/Program.cs
@@ -1,1 +1,16 @@
-Serve.Run(RunOptions.Default);
+//Serve.Run(RunOptions.Default);
+var builder = WebApplication.CreateBuilder(args).Inject();
+
+builder.Services.AddControllers().AddInject();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseInject();
+
+app.MapControllers();
+
+app.Run();
